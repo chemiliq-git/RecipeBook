fd05edb94316c2858801ef6e3e1f32ad4184873c
Madlena Kerinska <madbad@mail.bg>
2021-01-04 13:32:06 +0200

    Side bar view model is added

---
+++ src/Web/RecipeBook.Web.ViewModels/SideBarFilter/SideBarFilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeBook.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class SideBarFilterViewModel
+    {
+        public string SearchedText { get; set; }
+
+        public IEnumerable<SearchRecipeTypeViewModel> RecipeTypes { get; set; }
+
+        public IEnumerable<SearchIngredientTypeViewModel> IngredientTypes { get; set; }
+    }
+}
