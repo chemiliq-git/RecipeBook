0cddaa5a631c0a58008ba01e100c1c6d00076321
Madlena Kerinska <madbad@mail.bg>
2021-01-15 16:53:50 +0200

    CreateRecipeDataModel.cs is added

---
+++ src/Services/RecipeBook.Services.Data/CreateRecipeDataModel.cs
@@ -0,0 +1,28 @@
+﻿namespace RecipeBook.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+
+    using RecipeBook.Data.Models;
+    using RecipeBook.Services.Mapping;
+
+    public class CreateRecipeDataModel : IMapFrom<RecipeBook.Data.Models.Recipe>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string RecipeTypeId { get; set; }
+
+        public ICollection<IngredientRecipeType> IngredientRecipeTypes { get; set; }
+
+        public string IngredientSetId { get; set; }
+
+        public DateTime LastCooked { get; set; }
+    }
+}
+
