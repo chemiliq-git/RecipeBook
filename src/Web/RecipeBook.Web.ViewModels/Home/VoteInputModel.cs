e1108c2c7027bb88bd9069cd2846e9aeeb0705a8
Madlena Kerinska <madbad@mail.bg>
2021-01-22 12:11:55 +0200

    Vote input model is added

---
+++ src/Web/RecipeBook.Web.ViewModels/Home/VoteInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeBook.Web.ViewModels.Home
+{
+    using RecipeBook.Data.Models;
+
+    public class VoteInputModel
+    {
+        public string RecipeId { get; set; }
+
+        public VoteTypeEnm Type { get; set; }
+
+        public int Value { get; set; }
+    }
+}
